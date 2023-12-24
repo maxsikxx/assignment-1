dea95bb9fb17944e7b8df240c857ce17f204ee4f
maxsikxx <231324@astanait.edu.kz>
2023-12-24 13:20:05 +0600

    update Shape.java

--- src/models/Shape.java
+++ src/models/Shape.java
@@ -5,3 +5,20 @@ import java.util.ArrayList;
 import java.util.Map;
 
 public class Shape {
+
+    private ArrayList<Point> points = new ArrayList<Point>();
+
+    public Shape() {
+        points = new ArrayList<>();
+    }
+
+    public void addPoint(Point point) {  // This method adds a point to the points array.
+        points.add(point);
+    }
+
+    public double calculatePerimeter() {  // This method returns the sum of all lengths
+        double perimeter = 0;
+        int len = points.size();
+        for (int i = 0; i < len - 1; i++) {
+            perimeter += points.get(i).distance(points.get(i+1));
+        }
